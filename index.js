95362d4cdd9715be265a83a295a403c499d3bb1c
ElderAdewoye23 <adewoyee73@gmail.com>
2025-03-18 12:14:20 +0100

    My First Portfolio

---
+++ index.js
@@ -0,0 +1,13 @@
+
+
+
+
+const showSideBar = () => {
+ const sideBar = document.querySelector(".sideBar")
+ sideBar.style.display = "flex";
+}
+const hideSideBar = () => {
+    const sideBar = document.querySelector(".sideBar")
+    sideBar.style.display = "none";
+}
+
